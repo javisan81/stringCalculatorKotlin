2f30b0d367a1f5d2ec5117675c2bb98cc5adf2fd
Javier Lopez Fernandez <javierlopezfernandez@MacBook-Pro-de-Javier.local>
2023-01-17 19:50:37 +0100

    First valid version of the stringCalculator in kotlin

---
+++ src/test/kotlin/com/tw/stringcalculator/StringCalculatorTests.kt
@@ -0,0 +1,11 @@
+package com.tw.stringcalculator
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class StringCalculatorTests {
+
+    @Test fun `stupid test`() {
+        assertTrue(true);
+    }
+}
