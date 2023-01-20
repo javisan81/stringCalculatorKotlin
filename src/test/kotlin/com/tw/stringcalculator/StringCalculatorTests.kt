77f9b90bf22c2036d30c15126c0ebbf1cbb24f98
Javier Lopez Fernandez <javierlopezfernandez@MacBook-Pro-de-Javier.local>
2023-01-20 17:18:32 +0100

    add for 1

--- src/test/kotlin/com/tw/stringcalculator/StringCalculatorTests.kt
+++ src/test/kotlin/com/tw/stringcalculator/StringCalculatorTests.kt
@@ -5,11 +5,18 @@ import org.junit.jupiter.api.Test
 
 class StringCalculatorTests {
     fun add(numbers: String): Int {
-        return 0
+        if(numbers.isEmpty()){
+            return 0
+        }
+        return numbers.toInt()
     }
 
     @Test
     fun `should return 0 with an empty string`() {
         assertEquals(0, add(""))
+    }
+    @Test
+    fun `should return 1 for 1`() {
+        assertEquals(1, add("1"))
     }
 }
